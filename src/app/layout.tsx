43fa88304c610aba0ecdd2c86ab457e0f51cf3eb
bender <kudin.dima@gmail.com>
2026-04-29 00:43:42 +0000

    Update theme fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Tag from "@/tag/Tag";
 import { getVisualEditScript } from "@/utils/visual-edit-script";
 import { Inter_Tight } from "next/font/google";
 import { Open_Sans } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 
 
 
@@ -22,12 +23,13 @@ export const metadata: Metadata = {
 };
 
 
-const inter = Inter({
-  variable: "--font-inter",
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
 });
-const openSans = Open_Sans({
-  variable: "--font-open-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -39,7 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <ServiceWrapper>
-        <body className={`${inter.variable} ${openSans.variable} antialiased`}>
+        <body className={`${dmSans.variable} ${inter.variable} antialiased`}>
           <Tag />
           {children}
           <script
